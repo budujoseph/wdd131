80bb3f98bbb43f4c6d78c94c8cf28710009ecd5a
budujoseph <bud24002@byui.edu>
2024-10-22 15:21:25 +0000

    home html done

--- project/scripts/food.js
+++ project/scripts/food.js
@@ -3,7 +3,18 @@
 const hamburger = document.querySelector('nav');
 const list = document.querySelector('ul');
 
+const activePage = window.location.pathname;
+const navLinks = document.querySelectorAll('nav a');
+
+
 hamburger.addEventListener('click' ,() => {
     list.classList.toggle('show');
     hamburger.classList.toggle('show');
+})
+
+navLinks.forEach(link => {
+    if(link.href.includes(activePage)){
+        link.classList.add('active');
+
+    }
 })
