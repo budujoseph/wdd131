0d95f4d58d26fe1d4d473280d8f828bd052d50b7
budujoseph <bud24002@byui.edu>
2024-10-24 21:34:35 +0000

    contact

--- project/scripts/food.js
+++ project/scripts/food.js
@@ -7,13 +7,15 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll('nav a');
 
 
-hamburger.addEventListener('click' ,() => {
+
+
+hamburger.addEventListener('click', () => {
     list.classList.toggle('show');
     hamburger.classList.toggle('show');
 })
 
 navLinks.forEach(link => {
-    if(link.href.includes(activePage)){
+    if (link.href.includes(activePage)) {
         link.classList.add('active');
 
     }
@@ -25,7 +27,7 @@ const menuItems = [
         foodName: "BBQ Bacon Cheeseburger",
         foodPrice: "GH₵50.00",
         imageUrl:
-         "https://images.pexels.com/photos/552056/pexels-photo-552056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            "https://images.pexels.com/photos/552056/pexels-photo-552056.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         foodName: "The Magic Plate",
@@ -43,23 +45,22 @@ const menuItems = [
         foodName: "Hot Dog",
         foodPrice: "GH₵45.65",
         imageUrl:
-          "https://images.pexels.com/photos/3023479/pexels-photo-3023479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            "https://images.pexels.com/photos/3023479/pexels-photo-3023479.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         foodName: "Mighty Meat Feast",
         foodPrice: "GH₵80.25",
         imageUrl:
-          "https://images.pexels.com/photos/845812/pexels-photo-845812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            "https://images.pexels.com/photos/845812/pexels-photo-845812.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
     {
         foodName: "Ultimate Cheesy Crust",
         foodPrice: "GH₵70.00 ",
         imageUrl:
-          "https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            "https://images.pexels.com/photos/1647163/pexels-photo-1647163.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
     },
-  ];
+];
 
-  
 
 function createMenuSect(foodMenu) {
     foodMenu.forEach(menu => {
@@ -70,7 +71,7 @@ function createMenuSect(foodMenu) {
         let price = document.createElement('p');
 
         img.setAttribute('src', menu.imageUrl);
-        img.setAttribute('alt' ,`${menu.foodName}`);
+        img.setAttribute('alt', `${menu.foodName}`);
         img.setAttribute('loading', "lazy");
         img.setAttribute('width', '200px');
         name.textContent = menu.foodName;
@@ -82,8 +83,106 @@ function createMenuSect(foodMenu) {
 
         document.getElementById('menuItems').appendChild(sect);
 
-        
+
     });
 }
 
+
+
+
+const drink = document.getElementById('drinks');
+const dessert = document.getElementById('desserts');
+
+const refreshers = [
+
+    {
+        name: "Classic Lamonade",
+        price: "GH₵10.00",
+        category: "Drink",
+        imgURL:
+            "https://images.pexels.com/photos/2109099/pexels-photo-2109099.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        name: "Cocacola",
+        price: "GH₵17.00",
+        category: "Drink",
+        imgURL:
+            "https://images.pexels.com/photos/39720/pexels-photo-39720.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        name: "Celeb Cocktails",
+        price: "GH₵8.00",
+        category: "Drink",
+        imgURL:
+            "https://images.pexels.com/photos/2789328/pexels-photo-2789328.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+
+    {
+        name: "Vanilla Ice Cream",
+        price: "GH₵10.00",
+        category: "Dessert",
+        imgURL:
+            "https://images.pexels.com/photos/9227969/pexels-photo-9227969.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+
+    {
+        name: "Chocolate Lava Cake",
+        price: "GH₵16.00",
+        category: 'Dessert',
+        imgURL:
+            "https://images.pexels.com/photos/5372850/pexels-photo-5372850.jpeg?auto=compress&cs=tinysrgb&w=600",
+    },
+
+];
+
+function createRefreshersSect(fresherMenu) {
+    fresherMenu.forEach(fresher => {
+        let section = document.createElement('section');
+        section.classList.add('myfresher');
+
+        let img = document.createElement('img');
+        let name = document.createElement('h2');
+        let price = document.createElement('p');
+
+        img.setAttribute('src', fresher.imgURL);
+        img.setAttribute('alt', `${fresher.name}`);
+        img.setAttribute('loading', "lazy");
+        img.setAttribute('width', "200px")
+        name.textContent = fresher.name;
+        price.innerHTML = `<p><em>Price: </em><span>${fresher.price}</span></p>`;
+
+        section.appendChild(img);
+        section.appendChild(name);
+        section.appendChild(price);
+
+        // document.getElementById('#').appendChild(section);
+
+    });
+
+
+}
+
+
+
+drink.addEventListener('click', () => {
+    createRefreshersSect(refreshers.filter(fresher => fresher.category === "Drink"));
+    window.location.href = "drinks.html";
+});
+
+dessert.addEventListener('click', () => {
+    createRefreshersSect(refreshers.filter(fresher => fresher.category === "Dessert"));
+    window.location.href = "dessert.html";
+});
+
 document.addEventListener('DOMContentLoaded', createMenuSect(menuItems));
+document.addEventListener('DOMContentLoaded', createRefreshersSect(refreshers));
+
+
+
+
+
+
+
+
